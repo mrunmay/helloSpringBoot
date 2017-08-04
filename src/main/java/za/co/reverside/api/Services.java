72655116956f29df3621bf548a13b7a0bccaeaed
Mrunmaya Mohanty <Mrunmaya.Mohanty@fadv.com>
2017-08-04 12:04:10 +0530

    Added Delete all, and ResponseStatus

--- src/main/java/za/co/reverside/api/Services.java
+++ src/main/java/za/co/reverside/api/Services.java
@@ -2,10 +2,12 @@ package za.co.reverside.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.reverside.model.Employee;
 import za.co.reverside.repository.IEmployeeRepository;
@@ -13,6 +15,7 @@ import za.co.reverside.repository.IEmployeeRepository;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
 
 @RestController
@@ -23,6 +26,7 @@ public class Services
   IEmployeeRepository employeeRepository;
 
   @RequestMapping(value = "/add", method = RequestMethod.POST)
+  @ResponseStatus( HttpStatus.CREATED )
   public void addNewEmployee(@RequestBody Employee employee)
   {
     if (employee != null) {
@@ -31,6 +35,7 @@ public class Services
   }
 
   @RequestMapping(value = "employee/{id}", method = RequestMethod.GET, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK )
   public Employee getEmployee(@PathVariable("id") Long id)
   {
     Employee employee = employeeRepository.findOne(id);
@@ -42,25 +47,29 @@ public class Services
   }
 
   @RequestMapping(value = "employees", method = RequestMethod.GET, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK )
   public Collection<Employee> getEmployees()
   {
     return employeeRepository.findAll();
   }
 
   @RequestMapping(value = "employee/fname/{firstName}", method = RequestMethod.GET, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK )
   public Employee getEmployeeByFirstName(@PathVariable("firstName") String firstName)
   {
     return employeeRepository.getEmployeeByFirstName(firstName);
   }
 
   @RequestMapping(value = "employee/lname/{lastName}", method = RequestMethod.GET, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK )
   public Collection<Employee> getEmployeeByLastName(@PathVariable("lastName") String LastName)
   {
     return employeeRepository.getEmployeeByLastName(LastName);
   }
 
-  @RequestMapping(value = "employee/delete/{id}")
-  public void getRemoveEmployeeById(@PathVariable("id") Long id)
+  @RequestMapping(value = "employee/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK )
+  public void deleteEmployeeById(@PathVariable("id") Long id)
   {
     Employee employee = employeeRepository.findOne(id);
     if (employee == null)
@@ -70,13 +79,25 @@ public class Services
     employeeRepository.delete(id);
   }
 
-    @RequestMapping(path = "/build", method = RequestMethod.GET, produces = "application/json")
-    public Properties build() throws IOException
+  @RequestMapping(value = "employee/delete/all", method = RequestMethod.DELETE, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK)
+  public void deleteEmployees()
+  {
+    List<Employee> employees = employeeRepository.findAll();
+    if (employees == null || employees.isEmpty())
     {
-        InputStream in = new ClassPathResource("git.properties").getInputStream();
-        Properties properties = new Properties();
-        properties.load(in);
-        return properties;
+      throw new RuntimeException("No User Exist.");
     }
+    employeeRepository.deleteAll();
+  }
 
+  @RequestMapping(path = "/build", method = RequestMethod.GET, produces = "application/json")
+  @ResponseStatus( HttpStatus.OK)
+  public Properties build() throws IOException
+  {
+    InputStream in = new ClassPathResource("git.properties").getInputStream();
+    Properties properties = new Properties();
+    properties.load(in);
+    return properties;
+  }
 }
